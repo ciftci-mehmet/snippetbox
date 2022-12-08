8235ad8660ba0b40ea3d11e9a9a5d337c27b07d5
ciftci-mehmet <mciftci@maritimetrainer.com>
2022-12-08 17:25:39 +0300

    added show, create handlers

--- main.go
+++ main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-// define home handler
+// home handler
 func home(w http.ResponseWriter, r *http.Request) {
+
+	// return 404 if path not found
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+// showSnippet handler
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+
+	// get id parameter and check if valid
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// display snippet with id from parameter
+	fmt.Fprintf(w, "Display snippet with id: %d", id)
+}
+
+// createSnippet handler
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	// check if method is POST
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet"))
+}
+
 func main() {
-	// register home as a handler
+	// register handlers with corresponding URL patterns
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// start listen, serve and log any errors
 	log.Println("Starting server on :4000")
