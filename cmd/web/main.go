f74dedd67c93acefe1ba54d3d160ba45a0d5fdc6
ciftci-mehmet <mciftci@maritimetrainer.com>
2022-12-09 09:36:31 +0300

    added static files and file server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,10 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// create file server
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// start listen, serve and log any errors
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
