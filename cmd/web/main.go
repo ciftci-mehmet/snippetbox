d2b6bfab0135fde2d04abb3b5527f30f812314cb
ciftci-mehmet <mciftci@maritimetrainer.com>
2022-12-09 10:14:23 +0300

    logging improvements

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	// command line flag addr, default value :4000
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// parse command line flag
+	flag.Parse()
+
+	// new logs
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
 	// register handlers with corresponding URL patterns
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -16,9 +29,16 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// init server settings
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// start listen, serve and log any errors
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 
 }
