118fc08ce190f9ec3d2ff9cf25d5b3bc94cc5930
ciftci-mehmet <mciftci@maritimetrainer.com>
2022-12-21 17:18:58 +0300

    test createSnippetForm handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -122,3 +122,47 @@ func TestSignupUser(t *testing.T) {
 	}
 
 }
+
+func TestCreateSnippetForm(t *testing.T) {
+	// use mocked dependencies
+	app := newTestApplication(t)
+
+	// test server
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/snippet/create")
+		if code != http.StatusSeeOther {
+			t.Errorf("want %d; got %d", http.StatusSeeOther, code)
+		}
+		if headers.Get("Location") != "/user/login" {
+			t.Errorf("want %s; got %s", "/user/login", headers.Get("Location"))
+		}
+	})
+	t.Run("Authenticated", func(t *testing.T) {
+		// get csrf token from form
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		// post login
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+
+		// check if authenticated user is shown create snippet form
+		code, _, body := ts.get(t, "/snippet/create")
+		if code != http.StatusOK {
+			t.Errorf("want %d; got %d", http.StatusOK, code)
+		}
+
+		formTag := "<form action='/snippet/create' method='POST'>"
+		if !bytes.Contains(body, []byte(formTag)) {
+			t.Errorf("want body %s to contain %q", body, formTag)
+		}
+
+	})
+
+}
